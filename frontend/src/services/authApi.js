be7229649042edcfca5c3e3ad6a9f8cfa91de372
Ganesh Karadkar <karadkarganesh797@gmail.com>
2026-06-23 17:02:59 +0530

    Frontend ready for deployment

--- frontend/src/services/authApi.js
+++ frontend/src/services/authApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000";
+const API_URL = import.meta.env.VITE_API_URL;
 const api = axios.create({ baseURL: API });
 
 export const registerUser = (data) => api.post("/register", data);
--- frontend/src/services/birthdayApi.js
+++ frontend/src/services/birthdayApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000";
-const api = axios.create({ baseURL: API });
+const API_URL = import.meta.env.VITE_API_URL;
+const api = axios.create({ baseURL: API_URL });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
