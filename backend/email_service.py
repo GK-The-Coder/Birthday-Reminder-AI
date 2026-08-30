aa9a6e68a177787a369a64a327776aba6178c71f
Ganesh Karadkar <karadkarganesh797@gmail.com>
2026-08-30 14:24:54 +0530

    mail

--- backend/email_service.py
+++ backend/email_service.py
@@ -2,6 +2,7 @@ import os
 import smtplib
 from email.mime.text import MIMEText
 
+
 def send_email(receiver, subject, body):
     sender = os.getenv("EMAIL_ADDRESS")
     password = os.getenv("EMAIL_PASSWORD")
